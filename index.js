8e1e5d70d99fba95c35442e2c674373f665284ec
padmaja <gadyanavarpadmaja@gmail.com>
2021-09-21 17:51:49 +0530

    done

--- index.js
+++ index.js
@@ -1,26 +1,57 @@
 class SortedList {
   constructor() {
-  
+    this.items = []
+    this.length = 0
   }
-  add() {
-   
-  }
-  get() {
+  add(x) {
+    this.items.push(x)
 
+    this.items.sort((a,b)=>a-b)
+
+    this.length = this.items.length
+  }
+  get(i) {
+    if (this.items.indexOf(i) == -1) {
+      throw new Error('OutOfBounds')
+    }
+    else {
+      return(this.items.indexOf(i))
+    }
   }
   max() {
-
+    if(this.items.length>0)
+    {
+      return (Math.max(...this.items))
+    }
+    else{
+      throw new Error("EmptySortedList ")
+    }
   }
   min() {
-
+    if (this.items.length > 0) {
+      return (Math.min(...this.items))
+    }
+    else {
+      throw new Error("EmptySortedList ")
+    }
   }
   avg() {
-    
-   
+    if (this.items.length > 0) {
+      let sum = this.items.reduce((acc, val) =>acc= acc + val)
+      return sum/this.items.length
+      }
+    else {
+      throw new Error("EmptySortedList ")
+    }
   }
 
   sum() {
-  
+    if (this.items.length > 0) {
+     return this.items.reduce((acc, val) =>  acc + val)
+    }
+    else{
+      return 0
+    }
   }
   
 };
